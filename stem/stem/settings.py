5ca934b06f4243b90e0807e296afd41fe7901724
Erwol <ernes.wo@gmail.com>
2017-04-27 22:01:09 +0200

    mysql

--- stem/stem/settings.py
+++ stem/stem/settings.py
@@ -75,9 +75,11 @@ WSGI_APPLICATION = 'stem.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'db.cnf'),
+        },
     }
 }
 
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
--- stem/user/models.py
+++ stem/user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
+from django.utils import timezone
+from django.utils.http import urlquote
+from django.core.mail import send_mail
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+class Base(models.Model):
+    creation_date = models.DateTimeField(_("Fecha de creación"), auto_now_add=True)
+    modification_date = models.DateTimeField(_("Fecha de modificación"), auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class CustomUser(AbstractUser):
+    is_student = models.BooleanField(_("¿Es el usuario estudiante?"), default=True)
+    date_of_birth = models.DateField(_("Fecha de nacimiento"), default=timezone.now)
+
